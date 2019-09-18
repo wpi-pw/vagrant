df60b296b5451c89d4275ddfa1127a39b7adb9d8
DimaMinka <cdk@cdk.co.il>
2019-09-18 15:49:25 +0300

    chore--project--rebuild in progress 3

--- config/wpi-init.sh
+++ config/wpi-init.sh
@@ -1,17 +1,10 @@
 #!/usr/bin/env bash
 #
-# vagrant_up provisioning file
+# vagrant up provisioning file
 #
 # Author: Dima Minka
 #
 # File version 1.1.2
-
-# Define colors
-RED='\033[0;31m' # error
-GRN='\033[0;32m' # success
-BLU='\033[0;34m' # task
-BRN='\033[0;33m' # headline
-NC='\033[0m' # no color
 
 # Define custom config if exist
 if [[ -f wpi-custom.yml ]]; then
@@ -20,57 +13,8 @@ else
   wpi_config="/home/vagrant/config/wpi-default.yml"
 fi
 
-# YAML parser function
-parse_yaml() {
-    local yaml_file=$1
-    local prefix=$2
-    local s
-    local w
-    local fs
-
-    s='[[:space:]]*'
-    w='[a-zA-Z0-9_.-]*'
-    fs="$(echo @|tr @ '\034')"
-
-    (
-        sed -e '/- [^\“]'"[^\']"'.*: /s|\([ ]*\)- \([[:space:]]*\)|\1-\'$'\n''  \1\2|g' |
-
-        sed -ne '/^--/s|--||g; s|\"|\\\"|g; s/[[:space:]]*$//g;' \
-            -e "/#.*[\"\']/!s| #.*||g; /^#/s|#.*||g;" \
-            -e "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
-            -e "s|^\($s\)\($w\)${s}[:-]$s\(.*\)$s\$|\1$fs\2$fs\3|p" |
-
-        awk -F"$fs" '{
-            indent = length($1)/2;
-            if (length($2) == 0) { conj[indent]="+";} else {conj[indent]="";}
-            vname[indent] = $2;
-            for (i in vname) {if (i > indent) {delete vname[i]}}
-                if (length($3) > 0) {
-                    vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
-                    printf("%s%s%s%s=(\"%s\")\n", "'"$prefix"'",vn, $2, conj[indent-1],$3);
-                }
-            }' |
-
-        sed -e 's/_=/+=/g' |
-
-        awk 'BEGIN {
-                FS="=";
-                OFS="="
-            }
-            /(-|\.).*=/ {
-                gsub("-|\\.", "_", $1)
-            }
-            { print }'
-    ) < "$yaml_file"
-}
-
-# root user change
-noroot() {
-  sudo -EH -u "vagrant" "$@";
-}
-
-# Read config
-eval $(parse_yaml $wpi_config "conf_")
+# Get wpi-helper for yml parsing, noroot, errors etc
+source <(curl -s https://raw.githubusercontent.com/wpi-pw/template-workflow/master/wpi-helper.sh)
 
 if [ "$conf_vm_box" == "wpi/box" ]; then
   # Basic provison for new box
